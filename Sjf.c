84b0419c683edafe5dd4dca90383b7dfc9117481
Prajwal Kamath <125379835+prajwalkamath03@users.noreply.github.com>
2023-07-10 14:21:26 +0530

    Update Sjf.c

--- Sjf.c
+++ Sjf.c
@@ -1,53 +1,92 @@
-#include&lt;stdio.h&gt;
-int main()
+#include<stdio.h>
+void printer(int processes[],int n,int burst_time[],int arrival_time[])
 {
+    int WT[n],TAT[n],total_WT=0,total_TAT=0,completion_time[n],idle=0,RT[n];
+    for (int i = 0; i < n; i++)//calculating times
+    {
+        if(i==0)
+        {
+            WT[0]=0;
+            completion_time[0]=burst_time[0]+arrival_time[0];
+            TAT[0]=completion_time[0]-arrival_time[0];
+            total_TAT=TAT[0];
+            RT[0]=0;
+            if(arrival_time[0]!=0)
+            {
+                idle=arrival_time[0];
+            }
+            continue;
+        }
+        if(completion_time[i-1]<arrival_time[i])
+        {
+            completion_time[i]=arrival_time[i]+burst_time[i];
+            idle+=(arrival_time[i]-completion_time[i-1]);
+            RT[i]=0;
+        }
+        else
+        {
+            completion_time[i]=completion_time[i-1]+burst_time[i];
+            RT[i]=completion_time[i-1]-arrival_time[i];
+        }
+        TAT[i]=completion_time[i]-arrival_time[i];
+        total_TAT+=TAT[i];
+        WT[i]=TAT[i]-burst_time[i];
+        total_WT+=WT[i];
+    }
+    printf("Processes\tAT\tBT\tWT\tTAT\tCT\tRT\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t\t%d\t%d\t%d\t%d\t%d\t%d\n",processes[i],arrival_time[i],burst_time[i],WT[i],TAT[i],completion_time[i],RT[i]);
+    }
+    printf("AVG waiting time: %2f\n",(float)total_WT/n);
+    printf("AVG turnaround time: %2f\n",(float)total_TAT/n);
+    printf("Total Idle time: %d\n",idle);
+    printf("\n");
+}
+void Sorter(int n,int processes[],int burst_time[],int arrival_time[])
+{
+    for (int i = 0; i < n; i++)//sorting in priority
+    {
+        int first=i;
+        for (int j = i+1; j < n; j++)
+        {
+            if (burst_time[j]<burst_time[first])
+            {
+                first=j;
+            }
+        }
+        int temp=processes[i];
+        processes[i]=processes[first];
+        processes[first]=temp;
 
-int burst_time[20],p[20],waiting_time[20],tat[20],i,j,n,total=0,pos,temp;
-float avg_waiting_time,avg_tat;
-printf(&quot;please enter number of process: &quot;);
-scanf(&quot;%d&quot;,&amp;n);
-printf(&quot;\n enter the Burst Time:\n&quot;);
-for(i=0;i&lt;n;i++)
-{
-printf(&quot;p%d:&quot;,i+1);
-scanf(&quot;%d&quot;,&amp;burst_time[i]);
-p[i]=i+1;
-}
-// from here, burst times sorted
-for(i=0;i&lt;n;i++)
-{
-pos=i;
-for(j=i+1;j&lt;n;j++)
-{
-if(burst_time[j]&lt;burst_time[pos])
-pos=j;
-}
-temp=burst_time[i];
-burst_time[i]=burst_time[pos];
-burst_time[pos]=temp;
-temp=p[i];
-p[i]=p[pos];
-p[pos]=temp;
-}
+        temp=burst_time[i];
+        burst_time[i]=burst_time[first];
+        burst_time[first]=temp;
 
-waiting_time[0]=0;
-for(i=1;i&lt;n;i++)
-{
-waiting_time[i]=0;
-for(j=0;j&lt;i;j++)
-waiting_time[i]+=burst_time[j];
-total+=waiting_time[i];
+        temp=arrival_time[i];
+        arrival_time[i]=arrival_time[first];
+        arrival_time[first]=temp;
+    }
 }
-avg_waiting_time=(float)total/n;
-total=0;
-printf(&quot;\nProcess\t Burst Time \tWaiting Time\tTurnaround Time&quot;);
-for(i=0;i&lt;n;i++)
+void sjf(int processes[],int n,int burst_time[],int arrival_time[])
 {
-tat[i]=burst_time[i]+waiting_time[i];
-total+=tat[i];
-printf(&quot;\np%d\t\t %d\t\t %d\t\t\t%d&quot;,p[i],burst_time[i],waiting_time[i],tat[i]);
+    Sorter(n,processes,burst_time,arrival_time);
+    printf("SJF agorithm\n");
+    printer(processes,n,burst_time,arrival_time);
 }
-avg_tat=(float)total/n;
-printf(&quot;\n\n the average Waiting Time=%f&quot;,avg_waiting_time);
-printf(&quot;\n the average Turnaround Time=%f\n&quot;,avg_tat);
+void main()
+{
+    int n=0;
+    printf("Enter the number of processes: ");
+    scanf("%d",&n);
+    int processes[n],burst_time[n],arrival_time[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the arrival time for process %d:",i+1);
+        scanf("%d",&arrival_time[i]);
+        printf("Enter the Burst time for processes %d:",i+1);
+        scanf("%d",&burst_time[i]);
+        processes[i]=i+1;
+    }
+    sjf(processes,n,burst_time,arrival_time);
 }
