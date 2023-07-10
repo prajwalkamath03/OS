07086e1ed5bd6c6b4d2460a7da517271d582f7e1
Prajwal Kamath <125379835+prajwalkamath03@users.noreply.github.com>
2023-07-10 15:21:09 +0530

    Update roundrobin.c

--- Lab Program/roundrobin.c
+++ Lab Program/roundrobin.c
@@ -121,3 +121,31 @@ void main()
     scanf("%d",&quantum);
     roundRobin(processes,n,burst_time,quantum,arrival_time,prioritylist);
 }
+/* 
+OUTPUT
+----------------------------------------------------------------------
+    Enter the number of processes: 4
+Enter the arrival time for process 1:2
+Enter the Burst time for processes 1:1
+Enter the priority for process 1:1
+Enter the arrival time for process 2:1
+Enter the Burst time for processes 2:5
+Enter the priority for process 2:1
+Enter the arrival time for process 3:4
+Enter the Burst time for processes 3:1
+Enter the priority for process 3:1
+Enter the arrival time for process 4:0
+Enter the Burst time for processes 4:6
+Enter the priority for process 4:1
+Enter the time quantum for round robin: 2
+
+ROund RObin algorithm
+Processes       AT      BT      Priority        WT      TAT     CT      RT
+1               2       1       1               0       1       3       0
+2               1       5       1               7       12      13      2
+3               4       1       1               1       2       6       1
+4               0       6       1               6       12      12      0
+AVG waiting time: 3.500000
+AVG turnaround time: 6.750000
+AVG Response time: 0.750000
+Total Idle time: 0    */
