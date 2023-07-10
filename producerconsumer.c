63f8d20001402a94dac2f4925ea9b7eba28cb47f
Prajwal Kamath <125379835+prajwalkamath03@users.noreply.github.com>
2023-07-10 14:26:05 +0530

    Update producerconsumer.c

--- producerconsumer.c
+++ producerconsumer.c
@@ -1,61 +1,60 @@
-#include&lt;stdio.h&gt;
-#include&lt;stdlib.h&gt;
+#include<stdio.h>
+#include<stdlib.h>
 int mutex=1,full=0,empty=3,x=0;
-int main()
-{
-int n;
-void producer();
-void consumer();
-int wait(int);
-int signal(int);
-printf(&quot;\n1.Producer\n2.Consumer\n3.Exit&quot;);
-while(1)
-{
-printf(&quot;\nEnter your choice:&quot;);
-scanf(&quot;%d&quot;,&amp;n);
-switch(n)
-{
-case 1: if((mutex==1)&amp;&amp;(empty!=0))
-producer();
-else
-printf(&quot;Buffer is full!!&quot;);
-break;
-case 2: if((mutex==1)&amp;&amp;(full!=0))
-consumer();
-else
-printf(&quot;Buffer is empty!!&quot;);
-break;
-case 3:
-exit(0);
-break;
-}
-}
-return 0;
-}
 int wait(int s)
 {
-return (--s);
+    return (--s);
 }
-
 int signal(int s)
 {
-return(++s);
+    return(++s);
 }
 void producer()
 {
-mutex=wait(mutex);
-full=signal(full);
-empty=wait(empty);
-x++;
-printf(&quot;\nProducer produces the item %d&quot;,x);
-mutex=signal(mutex);
+    mutex=wait(mutex);
+    full=signal(full);
+    empty=wait(empty);
+    x++;
+    printf("\nProducer produces the item %d",x);
+    mutex=signal(mutex);
 }
 void consumer()
 {
-mutex=wait(mutex);
-full=wait(full);
-empty=signal(empty);
-printf(&quot;\nConsumer consumes item %d&quot;,x);
-x--;
-mutex=signal(mutex);
+    mutex=wait(mutex);
+    full=wait(full);
+    empty=signal(empty);
+    printf("\nConsumer consumes item %d",x);
+    x--;
+    mutex=signal(mutex);
+}
+int main()
+{
+    int n;
+    void producer();
+    void consumer();
+    int wait(int);
+    int signal(int);
+    printf("\n1.Producer\n2.Consumer\n3.Exit");
+    while(1)
+    {
+        printf("\nEnter your choice:");
+        scanf("%d",&n);
+        switch(n)
+        {
+            case 1:    if((mutex==1)&&(empty!=0))
+                        producer();
+                    else
+                        printf("Buffer is full!!");
+                    break;
+            case 2:    if((mutex==1)&&(full!=0))
+                        consumer();
+                    else
+                        printf("Buffer is empty!!");
+                    break;
+            case 3:
+                    exit(0);
+                    break;
+        }
+    }  
+    return 0;
 }
